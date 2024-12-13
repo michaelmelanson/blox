06fa1f22b39c8e95f7d6503991b4650aa1c2b59d
Michael Melanson <michael@michaelmelanson.net>
2024-12-13 14:44:51 -0500

    support x86_64 and aarch64 dev shells in nix config

--- flake.nix
+++ flake.nix
@@ -1,40 +1,46 @@
 {
-  inputs.nixpkgs.url = "nixpkgs/nixpkgs-unstable";
-
-  outputs = { self, nixpkgs }: let
-    pkgs = nixpkgs.legacyPackages.x86_64-darwin;
-  in {
-    devShells.x86_64-darwin.default = let
-      frameworks = pkgs.darwin.apple_sdk.frameworks;
-    in pkgs.mkShell {
-      buildInputs = [
-        pkgs.asciidoctor
-        pkgs.bash
-        pkgs.cargo
-        pkgs.cargo-outdated
-        pkgs.git
-        pkgs.iconv
-        pkgs.rustc
-        pkgs.tailwindcss
-        pkgs.tree-sitter
-        pkgs.lldb
-        frameworks.SystemConfiguration
-        frameworks.CoreServices
-      ];
-    };
-
-    packages.x86_64-darwin.default = pkgs.rustPlatform.buildRustPackage {
-      pname = "blox-server";
-      version = "0.1.0";
-      src = ./.;
-      cargoLock.lockFile = ./Cargo.lock;
-      cargoBuildFlags = [ "-p" "blox-server" ];
-    };
-
-    packages.x86_64-darwin.docs = pkgs.runCommand "docs" {
-        buildInputs = [ pkgs.asciidoctor ];
-    } ''
-        asciidoctor ${./.}/docs/index.adoc -D $out
-    '';
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem(system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = let
+          frameworks = pkgs.darwin.apple_sdk.frameworks;
+        in pkgs.mkShell {
+          buildInputs = [
+            pkgs.asciidoctor
+            pkgs.bash
+            pkgs.cargo
+            pkgs.cargo-outdated
+            pkgs.git
+            pkgs.iconv
+            pkgs.rustc
+            pkgs.tailwindcss
+            pkgs.tree-sitter
+            pkgs.lldb
+            frameworks.SystemConfiguration
+            frameworks.CoreServices
+          ];
+        };
+
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "blox-server";
+          version = "0.1.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          cargoBuildFlags = [ "-p" "blox-server" ];
+        };
+
+        packages.docs = pkgs.runCommand "docs" {
+            buildInputs = [ pkgs.asciidoctor ];
+        } ''
+            asciidoctor ${./.}/docs/index.adoc -D $out
+        '';
+      }
+    );
 }
